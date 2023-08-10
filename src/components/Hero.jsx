ea28d2a6b004beb023d3bbf827debe9a9f35b140
Divbyseptember <iamanthonyjay@gmail.com>
2023-08-10 10:53:02 -0400

    ...

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,6 @@
 import {motion} from 'framer-motion';
 import {style} from '../style'
-import {ComputersCanvas} from './canvas'
+// import {ComputersCanvas} from './canvas'
 
 const Hero = () => {
   return (
@@ -20,7 +20,7 @@ const Hero = () => {
         </div>
 
       </div>
-        <ComputersCanvas />
+        {/* <ComputersCanvas /> */}
          <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
             <a href="#about">
               <div className=" w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
